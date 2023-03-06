c3ebcc63ee475e7aff128343bf641c1ea0ee2825
Rahman E <rahman.erturk@gmail.com>
2023-03-06 10:24:37 +0100

    validate middleware added

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import * as controller from "../controllers/recipeController.js";
 
-// import { postSchema } from "./photoRoutes.schema.js";
-// import validate from "../middlewares/validate.js";
+import { postSchema } from "./recipeRouter.schema.js";
+import validate from "../middlewares/validate.js";
 
 const router = Router();
 
 router
   .get("/", controller.getAllRecipes)
   .get("/:recipeId", controller.getRecipe)
-  .post("/", /*validate(postSchema),*/ controller.createRecipe)
+  .post("/", validate(postSchema), controller.createRecipe)
   .patch("/:recipeId", /*validate(postSchema),*/ controller.updateRecipe)
   .put("/:recipeId", /*validate(postSchema),*/ controller.replaceRecipe)
   .delete("/:recipeId", controller.deleteRecipe);
