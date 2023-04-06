74a004f7d529bdbf8231c00515073848b3f159ba
Rahman E <rahman.erturk@gmail.com>
2023-04-06 08:42:16 +0200

    backend done

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
 import MongoStore from "connect-mongo";
+import multer from "multer";
+import path from "path";
+import fs from "fs/promises";
 
 import recipeRouter from "./routers/recipeRouter.js";
 import userRouter from "./routers/userRouter.js";
@@ -12,9 +15,9 @@ import authRouter from "./routers/authRouter.js";
 import logMiddleware from "./middlewares/log.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import upload from "./middlewares/uploadMiddleware.js";
 // import checkToken from "./middlewares/checkToken.js";
 
-import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import dotenv from "dotenv";
@@ -27,7 +30,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, "files/frontend", "uploads");
 
 app.use(
   session({
@@ -44,12 +49,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 
@@ -57,16 +62,39 @@ app.use(cookieParser());
 
 app.use(logMiddleware);
 
-// app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
-// app.get("/login", (req, res) => res.sendFile(__dirname + "/views/login.html"));
-// app.get("/register", (req, res) =>
-//   res.sendFile(__dirname + "/views/register.html")
-// );
-
 app.use("/api/recipes", recipeRouter);
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
+
+app.post("/api/upload-image", upload.single("image"), async (req, res) => {
+  try {
+    res.status(200).json({
+      fileName: req.file.filename,
+      filePath: "/uploads/" + req.file.filename,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/api/delete-image/:fileName", async (req, res) => {
+  const { fileName } = req.params;
+  const filePath = path.join(uploadDir, fileName);
+  try {
+    await fs.unlink(filePath);
+    res.status(200).json({ message: "File deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // app.use("/api/courses", courseRouter);
+app.use("/", express.static("./files/frontend"));
+app.get("/*", (req, res) =>
+  res.sendFile(__dirname + "/files/frontend/index.html")
+);
 
 app.use(notFoundHandler);
 
